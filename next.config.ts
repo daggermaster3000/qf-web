82abf57d03287888315250c288c123d9ecc13e5c
daggermaster3000 <quillan.favey@gmail.com>
2024-11-05 12:24:07 +0100

    aaa

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,7 @@
 import type { NextConfig } from "next";
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = ''
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
-
 const nextConfig: NextConfig = {
-  output: 'export',
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    unoptimized: true,
-  },
+
 };
 
 export default nextConfig;
