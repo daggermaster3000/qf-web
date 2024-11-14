dd5b4295d9657102da53119179e9b6d0b1bf02ae
daggermaster3000 <quillan.favey@gmail.com>
2024-11-14 17:33:27 +0100

    updated image paths

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,21 +10,21 @@ const codingProjects = [
     {
         title: 'Plotdelice',
         description: 'A small collection of functions for custom plots in python',
-        imageUrl: '/plotdelice.png',
+        imageUrl: 'https://raw.githubusercontent.com/daggermaster3000/plotdelice/refs/heads/main/assets/image.png',
         link: 'https://github.com/daggermaster3000/plotdelice',
         id: 'plotdelice'
     },
     {
         title: 'Cerebroflow',
         description: 'A python library to analyze csf flow in zebrafish larvae using a kymograph approach',
-        imageUrl: 'https://private-user-images.githubusercontent.com/82659911/307736313-78bd6876-96be-4d15-b8d8-577b9c4d4cc5.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MzE0NTA1MDUsIm5iZiI6MTczMTQ1MDIwNSwicGF0aCI6Ii84MjY1OTkxMS8zMDc3MzYzMTMtNzhiZDY4NzYtOTZiZS00ZDE1LWI4ZDgtNTc3YjljNGQ0Y2M1LnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDExMTIlMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQxMTEyVDIyMjMyNVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWRkMzA2MGU4MjMwMmQ1NTAwOTE2MDk0YWQ0Y2UxNjkzN2M2ODBjOWI4MGExOTA3MDllMmQ1Y2U1NzZmMDdkZDkmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.HDwVEEUncTjXPMmPCM8RaxwM-KD8eCyuxUu00wTeEW4',
+        imageUrl: 'https://github.com/daggermaster3000/CerebroFlow/assets/82659911/78bd6876-96be-4d15-b8d8-577b9c4d4cc5',
         link: 'https://github.com/daggermaster3000/Cerebroflow',
         id: 'cerebroflow'
     },
     {
         title: 'Anisofish',
         description: 'A python repository to analyze zebrafish muscle fiber anisotropy using a 2D wavelet transform modulus maxima approach',
-        imageUrl: '/anisofish.png',
+        imageUrl: 'https://github.com/user-attachments/assets/0901b5ed-e88c-408c-8632-6503b47367cc',
         link: 'https://github.com/daggermaster3000/AnisoFish',
         id: 'anisofish'
     }
@@ -34,7 +34,7 @@ const researchProjects = [
     {
         title: 'Neuroscience Msc Thesis',
         description: 'Investigating body axis curvature in zebrafish ciliopathy models using a quantitative approach. An in-depth analysis of CSF flow and muscle fiber anisotropy in zebrafish models.',
-        imageUrl: '/fish.jpg',
+        imageUrl: '/qf-portfolio/fish.jpg',
         link: 'https://digitalposterbachmannlab.netlify.app/',
         id: 'csf-flow-dynamics'
     },
